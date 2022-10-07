ebf1939a5dc7bdaf9cad473b31695763b713cfcb
PhyoeZawAung <phyoezawaung9696@gmail.com>
2022-10-07 11:05:01 +0630

    to merge

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>This is the develop branch</Text>
+      <Text>Merging</Text>
       <StatusBar style="auto" />
     </View>
   );
